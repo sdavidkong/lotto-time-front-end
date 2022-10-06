e07a57c84a4c96e6c3ea0a6ffc27596f677238d9
S David Kong <SDAVIDKONG@gmail.com>
2022-10-05 21:12:53 -0400

    added constants

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useWeb3Contract } from "react-moralis";
 
 const LotteryEntrance = () => {
+  // const {runContractFunction: enterRaffle} = useWeb3Contract({
+  //   abi://,
+  //   contractAddress: //,
+  //   params: {},
+  //   msgValue:
+
+  // })
+
   return <div>LotteryEntrance</div>;
 };
 

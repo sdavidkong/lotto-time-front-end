5314916eee5aaefa7205f3940ee44c9692859dc1
S David Kong <SDAVIDKONG@gmail.com>
2022-10-09 14:07:52 -0400

    added banner img

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,14 @@ import React from "react";
 const Header = () => {
   return (
     <div className="border-b-2 p-5 flex flex-row">
-      <h1 className="py-4 px-4 font-blog text-3xl">Lotto Time!</h1>
+      <div>
+        <img
+          className="lottoTimeBanner"
+          src="/lottotime.png"
+          alt="A colorful banner that reads Lotto Time!"
+        />
+      </div>
+
       <ConnectButton moralisAuth={false} />
     </div>
   );
